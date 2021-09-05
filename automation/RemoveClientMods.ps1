fa39a61937c802ccc96551e7b32794de6c865c85
Stijn Martens <doomednoctis@gmail.com>
2021-09-05 12:10:51 +0200

    v0.07 Added many mods

--- automation/RemoveClientMods.ps1
+++ automation/RemoveClientMods.ps1
@@ -1,7 +1,9 @@
 # List of Mods to remove for the Server
 # In the format @("filename", "filename")
 $CLIENT_MODS_TO_REMOVE_FROM_SERVER_FILES = @(
-	"farsight"
+	"farsight",
+    "InventoryHud",
+    "Neat"
 )
 
 $InstanceRoot = ("$PSScriptRoot/.." | Resolve-Path)
